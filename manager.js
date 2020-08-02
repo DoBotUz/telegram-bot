d16cd3af65f8cc271635ae7bc85a6f51005e7456
unknown <hubzein@gmail.com>
2020-08-02 16:25:21 +0500

    refactor koa middleware

--- manager.js
+++ manager.js
@@ -5,24 +5,30 @@ const configure = require('./bot');
 const dbService = require('./services/db');
 const { webhook } = require('./config');
 
+const DOBOT_TOKENS = [];
+const DOBOTS = {};
+
 const app = new Koa();
 app.use(KoaBody());
+app.use(async (ctx, next) => {
+  let token = ctx.url.replace(webhook.path, '');
+  if (ctx.method === 'POST' && DOBOT_TOKENS.some(t => t === token)) {
+    await DOBOTS[token].handleUpdate(ctx.request.body, ctx.response);
+  }
+  ctx.status = 200;
+  next();
+});
 
 function attachBot(meta) { // meta = bot
   const bot = new Telegraf(meta.token)
-  app.use(async (ctx, next) => {
-    if (ctx.method === 'POST' && ctx.url.replace(webhook.path, '') === meta.token) {
-      await bot.handleUpdate(ctx.request.body, ctx.response);
-    }
-    ctx.status = 200;
-    next();
-  })
   bot.use((ctx, next) => {
     ctx.meta = meta;
     next();
   });
   configure(bot);
   bot.telegram.setWebhook(webhook.url + webhook.path + meta.token);
+  DOBOTS[meta.token] = bot;
+  DOBOT_TOKENS.push(meta.token);
 }
 
 dbService('bot').where({
