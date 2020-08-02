cfc6902dede98649d16ad7276be23b8710bf5553
unknown <hubzein@gmail.com>
2020-08-02 16:59:52 +0500

    add stub middleware

--- bot.js
+++ bot.js
@@ -2,7 +2,11 @@ require('./routes');
 const session = require('./middlewares/session');
 
 function configure(bot) {
-  bot.use(session.middleware())
+  bot.use(session.middleware());
+  bot.use((ctx, next) => {
+    ctx.reply('Загрузка...');
+    next();
+  })
   bot.start((ctx) => global.routes.start(ctx));
 }
 
--- routes/start.js
+++ routes/start.js
@@ -1,7 +1,7 @@
 const Markup = require('telegraf/markup');
 
 module.exports = ctx => {
-  ctx.replyWithMarkdown(
+  return ctx.replyWithMarkdown(
     'Вкусный плов в офис или на дом',
     Markup.keyboard([
       '📙 Меню', 'ℹ️ Инфо',
